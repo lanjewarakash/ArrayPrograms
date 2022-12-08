886655546d46cbba077846e7ba7c8c821dec0042
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-08 11:57:49 +0530

    Added : Sort the elements of an array in descending order

---
+++ src/com/blz/array/DescendingArray.java
@@ -0,0 +1,28 @@
+package com.blz.array;
+
+public class DescendingArray {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        DescendingArray obj = new DescendingArray();
+        obj.sortArray(arr);
+        obj.printArray(arr);
+    }
+
+    public void sortArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    arr[i] = arr[i] + arr[j];
+                    arr[j] = arr[i] - arr[j];
+                    arr[i] = arr[i] - arr[j];
+                }
+            }
+        }
+    }
+
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
